f1556f14cfcf0cc783a639e5abda0604ecc6fc1d
Teyik0 <teyik0.fr@icloud.com>
2023-04-28 15:24:23 +0200

    project section update

--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -1,28 +1,37 @@
-'use client';
+"use client";
 
-/* eslint-disable @next/next/no-img-element */
-import { useState } from 'react';
+import Link from "next/link";
+import { useState } from "react";
+import { BsGithub, BsYoutube } from "react-icons/bs";
+import { TbWorldWww } from "react-icons/tb";
 
 interface IProps {
   name: string;
-  category: string;
+  category: string[];
   cover: string;
+  url?: {
+    project?: string;
+    youtube?: string;
+    github?: string;
+  };
 }
 
-const ProjectItem = ({ name, category, cover }: IProps) => {
+const ProjectItem = ({ name, category, cover, url }: IProps) => {
   const [hover, setHover] = useState(false);
+  const categoryStyle = `text-white py-2 px-4 bg-[#4c436e] rounded-full lg:text-[1.5vh] sm:text-[10px] text-[8px]`;
+  const urlStyle = `hover:text-[#775e28] duration-300 ease-in-out`;
   return (
     <div
       className={`h-[20vh] w-[40vh] lg:h-[30vh] lg:w-[60vh] 
-        rounded-3xl cursor-pointer flex justify-center items-center
+        rounded-3xl flex justify-center items-center
         ${cover} bg-cover bg-center bg-no-repeat
-        ease-in-out duration-500 overflow-hidden relative z-0 hover:scale-110
+        ease-in-out duration-500 overflow-hidden relative z-0 hover:scale-105
 
         before:bg-[#75A1A1] before:opacity-70 before:absolute before:h-full before:w-full 
         before:translate-x-[100%] before:rounded-l-full before:transition-all 
-        before:duration-700 before:ease-in-out hover:before:translate-x-0 hover:before:rounded-none
+        before:duration-500 before:ease-in-out hover:before:translate-x-0 hover:before:rounded-none
 
-        hover:after:content-['Try_this_out'] after:duration-1000 after:ease-in-out after:translate-x-[100%]
+        hover:after:content-['Check_this_out'] after:duration-500 after:ease-in-out after:translate-x-[100%]
         hover:after:translate-x-0 hover:after:z-40 hover:after:absolute hover:after:uppercase
         after:font-bold after:tracking-wider
         `}
@@ -30,17 +39,53 @@ const ProjectItem = ({ name, category, cover }: IProps) => {
       onMouseOutCapture={() => setHover(false)}
     >
       {hover && (
-        <img
-          src='/img/ajouter.png'
-          alt='en savoir plus'
-          className='h-[100px] w-[100px] md:h-[10vh] md:w-[10vh] p-4'
-        />
+        <div className="absolute top-4 left-4 flex gap-2 flex-wrap">
+          {category.map((item, index) => (
+            <span key={index} className={categoryStyle}>
+              {item}
+            </span>
+          ))}
+        </div>
+      )}
+      {hover && (
+        <div className="absolute bottom-4 left-4 flex gap-4 flex-wrap items-center">
+          {url?.github && (
+            <Link href={url.github} target="_blank">
+              <BsGithub
+                key={url.github}
+                className={`${urlStyle} lg:text-[3.2vh] sm:text-[25px] text-[15px]`}
+              />
+            </Link>
+          )}
+          {url?.youtube && (
+            <Link href={url.youtube} target="_blank">
+              <BsYoutube
+                key={url.youtube}
+                className={`${urlStyle} lg:text-[4vh] sm:text-[30px] text-[20px]`}
+              />
+            </Link>
+          )}
+          {url?.project && (
+            <Link href={url.project} target="_blank">
+              <TbWorldWww
+                key={url.project}
+                className={`${urlStyle} lg:text-[4vh] sm:text-[30px] text-[20px]`}
+              />
+            </Link>
+          )}
+        </div>
       )}
       <h2
-        className='absolute bottom-0 right-0 p-2 bg-gray-800 rounded-l-full text-white uppercase tracking-widest
-        lg:text-[1.5vh] sm:text-[10px] text-[8px]'
+        className="absolute bottom-0 right-0 p-2 bg-gray-800 rounded-l-full text-white uppercase tracking-widest
+        lg:text-[1.5vh] sm:text-[10px] text-[8px] cursor-pointer hover:text-[#775e28] duration-300 ease-in-out"
       >
-        {name}
+        {url?.project ? (
+          <Link href={url.project} target="_blank">
+            {name}
+          </Link>
+        ) : (
+          name
+        )}
       </h2>
     </div>
   );
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,66 +1,96 @@
-import ProjectItem from './ProjectItem';
-import Link from 'next/link';
+import ProjectItem from "./ProjectItem";
+
+const enhancedchatgptclone = {
+  project: "https://enhanced-gpt.netlify.app/",
+  github: "https://github.com/Teyik0/Enhanced-ChatGPT-Clone",
+  youtube:
+    "https://www.youtube.com/watch?v=logTaDXg-p4&ab_channel=Th%C3%A9oSamarasinghe",
+};
+const krypt = {
+  project: "https://kryptongoerli.netlify.app/",
+};
+const indiancoffee = {
+  project: "https://indiancoffee.fr/",
+};
+const srikastore = {
+  youtube: "https://youtu.be/ecVZBJZusk0",
+  github: "https://github.com/Teyik0/srika-store",
+};
+const epitact = {
+  youtube: "https://youtube.com/shorts/e7I4HjtPEDA?feature=share",
+};
+const robotsuiveur = {
+  project: "https://theo-old-portfolio.netlify.app/portfolio/robot-suiveur",
+};
+const laserChat = {
+  project: "https://theo-old-portfolio.netlify.app/portfolio/laser-pour-chat",
+};
+const listx = {
+  project: "https://theo-old-portfolio.netlify.app/portfolio/listx",
+};
 
 const Projects = () => {
   return (
-    <section className='flex flex-col h-[100vh] md:h-full md:justify-center md:items-center'>
-      <h1 className='md:hidden uppercase text-center text-white text-5xl pt-2'>
+    <section className="flex flex-col h-[100vh] md:h-full md:justify-center md:items-center">
+      <h1 className="md:hidden uppercase text-center text-white text-5xl pt-2">
         Projects
       </h1>
 
-      <div className='flex flex-wrap gap-8 justify-center py-4 overflow-auto'>
-        <Link href='https://enhanced-gpt.netlify.app/' target='_blank'>
-          <ProjectItem
-            name='Enhanced ChatGPT Clone'
-            category='test'
-            cover="bg-[url('/img/enhancedgptchat.png')]"
-          />
-        </Link>
-        <Link href='https://kryptongoerli.netlify.app/' target='_blank'>
-          <ProjectItem
-            name='Krypt - (Crypto Goerli Testnet)'
-            category='test'
-            cover="bg-[url('/img/Web3-0.png')]"
-          />
-        </Link>
-        <Link href='https://indiancoffee.fr/' target='_blank'>
-          <ProjectItem
-            name='Indian Coffee'
-            category='test'
-            cover="bg-[url('/img/Indian-Coffe.png')]"
-          />
-        </Link>
-        <Link href='https://youtu.be/ecVZBJZusk0' target='_blank'>
-          <ProjectItem
-            name='Srika Store'
-            category='test'
-            cover="bg-[url('/img/ecommerce/im1.png')]"
-          />
-        </Link>
-        <Link
-          href='https://youtube.com/shorts/e7I4HjtPEDA?feature=share'
-          target='_blank'
-        >
-          <ProjectItem
-            name='Epitact App - React Native'
-            category='test'
-            cover="bg-[url('/img/Epitact.jpg')]"
-          />
-        </Link>
+      <div className="flex flex-wrap gap-8 justify-center py-4 overflow-auto">
         <ProjectItem
-          name='Robot Suiveur'
-          category='test'
+          name="Enhanced ChatGPT Clone"
+          category={["NextJS13", "Prisma", "TailwindCSS", "Typescript"]}
+          cover="bg-[url('/img/enhancedgptchat.png')]"
+          url={enhancedchatgptclone}
+        />
+        <ProjectItem
+          name="Krypt - (Crypto Goerli Testnet)"
+          category={["NextJS12", "TailwindCSS", "Javascript", "Solidity"]}
+          cover="bg-[url('/img/Web3-0.png')]"
+          url={krypt}
+        />
+        <ProjectItem
+          name="Indian Coffee"
+          category={["NextJS12", "TailwindCSS", "Typescript"]}
+          cover="bg-[url('/img/Indian-Coffe.png')]"
+          url={indiancoffee}
+        />
+        <ProjectItem
+          name="Srika Store"
+          category={[
+            "NextJS12",
+            "TailwindCSS",
+            "Typescript",
+            "Stripe",
+            "Sanity",
+            "Zustand",
+          ]}
+          cover="bg-[url('/img/ecommerce/im1.png')]"
+          url={srikastore}
+        />
+        <ProjectItem
+          name="Epitact - React Native"
+          category={["React Native", "Typescript", "Jotai"]}
+          cover="bg-[url('/img/Epitact.jpg')]"
+          url={epitact}
+        />
+        <ProjectItem
+          name="Robot Suiveur"
+          category={["Embarqué", "Arduino", "C"]}
           cover="bg-[url('/img/robot_suiveur/robotsuiveur.png')]"
+          url={robotsuiveur}
         />
         <ProjectItem
-          name='Laser automatique pour chat'
-          category='test'
+          name="Laser automatique pour chat"
+          category={["Embarqué", "Arduino", "C"]}
           cover="bg-[url('/img/laser_chat/laserchat.png')]"
+          url={laserChat}
         />
         <ProjectItem
-          name='ListX - Base de donnée utilisateur'
-          category='test'
+          name="ListX - Base de donnée utilisateur"
+          category={["Qt Framework", "C++"]}
           cover="bg-[url('/img/listx/listx.png')]"
+          url={listx}
         />
       </div>
     </section>
